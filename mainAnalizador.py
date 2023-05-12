396add8158d1f1a81ac47a9ce67af6f0b9de62fb
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-05-12 16:44:38 -0500

    Update mainAnalizador.py

---
+++ mainAnalizador.py
@@ -0,0 +1,7 @@
+'''
+    Desarrollado por: Valentina Oviedo Sanchez, Orlando Narvaez Baracaldo y Eliana Yiset Hernandez Ortiz :)
+    Aginatura: TLF
+    Nombre: Analizador lexico
+    Cadena a evaluar:
+    menmaydifigumenigumayiguyonegiguigusumiguresigumuligudivigumodigu˄˅<>EntRealCadenaCaracterPublicoPrivado$Persona1sumresdivmodmul15E3.5R%C%#C#~ciclodecidetipoAla1ala1@12ABCDEFHex
+'''
